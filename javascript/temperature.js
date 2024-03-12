844da92ab624c815b835068fdaa8052d536e10c2
Kavinkumar.baskar <kavinkumar.baskar@cdw.com>
2024-03-12 23:19:06 +0530

    implemented-initial-code-splitting

---
+++ javascript/temperature.js
@@ -0,0 +1,23 @@
+/**
+ * Function to fetch and update temperature for each minute
+ */
+ const fetchTemperature = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${apiKey}`
+      );
+      if (!response.ok) {
+        throw new Error(`Error fetching weather data: ${response.status}`);
+      }
+      const weatherData = await response.json();
+      const tempInKelvin = weatherData.main.temp;
+      const tempInCelsius = tempInKelvin - 273.15;
+  
+      dashboardTemperature.text(`${tempInCelsius.toFixed(1)} °C`)
+    } catch (error) {
+      console.error("Error:", error);
+    }finally{
+      // Schedule the next update after 1 minute (60 seconds)
+      setTimeout(fetchTemperature, ONE_MINUTE);
+    }
+  };
