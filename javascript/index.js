ca179ff87eea4b8030b7f570029030d948b0240d
Kavinkumar.baskar <kavinkumar.baskar@cdw.com>
2024-03-13 00:06:55 +0530

    implemented-steering-functionality

--- javascript/index.js
+++ javascript/index.js
@@ -11,7 +11,7 @@ const video = document.getElementById("backCamera");
 const speedWarning = $("#speedWarning");
 const dashboardTime = $("#dashboardTime");
 const dashboardTemperature = $("#dashboardTemrature");
-const carSteeringWheel = $("#carSteeringWheel");
+const carSteeringWheel = document.getElementById("carSteeringWheel");
 
 //BUTTONS
 const startBtn = $("#startBtn");
@@ -46,6 +46,7 @@ let isCameraOn = false;
 let fuel = 60;
 let speed = 0;
 let decreaseFn;
+let currentAngle = 0;
 
 /**
  * Function to toggle startscreen and gamescreen
